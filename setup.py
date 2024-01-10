48a688994ecba0cc1cf14a9f9b6babe8e4b86e50
Pratik <pratikmahadik2524@gmail.com>
2024-01-10 15:42:55 +0530

    CookierCutter_initial_commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Demo project using cookie cutter',
+    author='Pratik mahadik',
+    license='',
+)
